9b5448de9b6351a61cfc0ec6cce2d27e7145a2a4
Alexandre Lima <alexandreatlima@gmail.com>
2023-02-28 14:01:51 -0300

    aula context e auth frontend

---
+++ src/pages/Login/index.js
@@ -0,0 +1,58 @@
+import { useContext, useState } from "react";
+import { api } from "../../api/api";
+import { AuthContext } from "../../context/authContext";
+
+export function Login() {
+  const { setLoggedInUser } = useContext(AuthContext);
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/user/login", form);
+      setLoggedInUser(response.data);
+
+      localStorage.setItem("loggedInUser", JSON.stringify(response.data));
+
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="input-email">E-mail:</label>
+        <input
+          id="input-email"
+          type="email"
+          value={form.email}
+          name="email"
+          onChange={handleChange}
+        />
+        <label htmlFor="input-password">Senha:</label>
+        <input
+          id="input-password"
+          type="password"
+          value={form.password}
+          name="password"
+          onChange={handleChange}
+        />
+
+        <button>Entrar!</button>
+      </form>
+    </>
+  );
+}
---
+++ src/pages/Signup/index.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../api/api";
+
+export function Signup() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await api.post("/user/signup", form);
+
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="input-name">Nome:</label>
+      <input
+        id="input-name"
+        type="text"
+        value={form.name}
+        name="name"
+        onChange={handleChange}
+      />
+      <label htmlFor="input-email">E-mail:</label>
+      <input
+        id="input-email"
+        type="email"
+        value={form.email}
+        name="email"
+        onChange={handleChange}
+      />
+      <label htmlFor="input-password">Senha:</label>
+      <input
+        id="input-password"
+        type="password"
+        value={form.password}
+        name="password"
+        onChange={handleChange}
+      />
+
+      <button>Cadastrar!</button>
+    </form>
+  );
+}
